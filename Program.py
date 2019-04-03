5654cb6abd321828a67b4a4792f11666dc6b3566
Obywatel92 <michalwr@op.pl>
2019-04-03 10:47:50 +0200

    wyznaczanie macierzy sztywnosci (K)

--- Program.py
+++ Program.py
@@ -77,6 +77,8 @@ class Zadanie:
 		#TODO: Wczytac warunki poczatkowe z pliku - na razie sa na sztywno
 		self.T = numpy.zeros((len(self.wspolrzedneWezlow), 1))
 		self.T += 600.0
+		
+		#self.T[0] = 300.0
 					
 	def wypisz(self) :
 		print('Wezly')
@@ -95,7 +97,7 @@ class Zadanie:
 		print(self.macierzM)
 		
 		print('Macierz sztywnosci')
-		print(self.macierzM)
+		print(self.macierzK)
 		
 			
 	def utworz_macierz(self) :
@@ -103,25 +105,57 @@ class Zadanie:
 		self.macierzM = numpy.zeros((liczba_wezlow, liczba_wezlow))
 		self.macierzK = numpy.zeros((liczba_wezlow, liczba_wezlow))
 		
+		# Przepisanie wspolrzednych do osobnych tablic, zeby kod byl bardziej czytelny
+		x = []
+		y = []
+		for wezel in self.wspolrzedneWezlow :
+			x.append(wezel[0])
+			y.append(wezel[1])
+
+		
 		for obszar in self.koneksje :
 			# Pole obszaru - ze wzoru na pole trojkata o danych wierzcholkach
-			a1 = self.wspolrzedneWezlow[obszar[0]][0]
-			a2 = self.wspolrzedneWezlow[obszar[0]][1]
-			b1 = self.wspolrzedneWezlow[obszar[1]][0]
-			b2 = self.wspolrzedneWezlow[obszar[1]][1]
-			c1 = self.wspolrzedneWezlow[obszar[2]][0]
-			c2 = self.wspolrzedneWezlow[obszar[2]][1]
-			a = numpy.abs(a1*b2+b1*c2+c1*a2-c1*b2-a1*c2-b1*a2) / 2.0
+			a1 = x[obszar[0]]
+			a2 = y[obszar[0]]
+			b1 = x[obszar[1]]
+			b2 = y[obszar[1]]
+			c1 = x[obszar[2]]
+			c2 = y[obszar[2]]
+			a = numpy.abs(a1*b2+b1*c2+c1*a2-c1*b2-a1*c2-b1*a2) / 2.0		
 			
 			for wezel1 in obszar :
 				for wezel2 in obszar :
+					# Robimy macierz "lumped". Gdyby to była ta zwykla, to byloby "a / 6.0" i "a / 12.0"
 					if (wezel1 == wezel2) :
-						self.macierzM[wezel1][wezel2] += a / 6.0
-					else :
 						self.macierzM[wezel1][wezel2] += a / 12.0
+					else :
+						self.macierzM[wezel1][wezel2] += 0
 						
-					#TODO: Wypelnic takze macierzK
+			for i in range(len(obszar)) :
+				for j in range(len(obszar)) :
+					# i oraz j to wspolrzedne lokalne (0, 1, 2)
 					
+					# Dla i oraz j trzeba znalezc indeksy sasiednich wezlow
+					# Przykladowo, jesli i = 2, to i_m_1 = 1 a  i+1 = 0 (bo doszlismy do konca tablicy) 
+					i_p_1 = (i+1) % len(obszar)					
+					i_m_1 = i-1
+					if i_m_1 < 0 :
+						i_m_1 = len(obszar)-1
+						
+					j_p_1 = (j+1) % len(obszar)
+					j_m_1 = j-1
+					if j_m_1 < 0 :
+						j_m_1 = len(obszar)-1						
+						
+					# Obliczamy wartosci C
+					c2i = y[obszar[i_p_1]] - y[obszar[i_m_1]]
+					c2j = y[obszar[j_p_1]] - y[obszar[j_m_1]]
+					
+					c3i = x[obszar[i_m_1]] - x[obszar[i_p_1]]			
+					c3j = x[obszar[j_m_1]] - x[obszar[j_p_1]]
+					
+					#Wypelniamy macierzK
+					self.macierzK[obszar[i]][obszar[j]] += (c2i*c2j + c3i*c3j) / (4.0 * a)				
 	
 						
 	def krok(self, dt) :
@@ -132,7 +166,7 @@ class Zadanie:
 		A = self.macierzM
 				
 		a = self.l / (self.c * self.p)		
-		MK = self.macierzM - (self.macierzK*a*dt)		
+		MK = self.macierzM - (self.macierzK*a*dt)
 		B = numpy.matmul(MK, self.T)
 
 		x = numpy.linalg.solve(A, B)
@@ -149,4 +183,5 @@ if __name__ == "__main__":
 	mes.utworz_macierz()	
 	mes.wypisz()
 	
+	mes.krok(0.1)
 	mes.krok(0.1)
